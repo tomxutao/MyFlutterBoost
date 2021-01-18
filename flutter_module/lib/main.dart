7a9e4257a25d13ac08abcdbe7a739bb126004a22
tomyxu <tomyxu@tencent.com>
2021-01-18 18:57:27 +0800

    将flutterboost非插件形式导入自己工程

---
+++ flutter_module/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'flutterboost/flutter_boost.dart';
+import 'simple_page_widgets.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+
+    FlutterBoost.singleton.registerPageBuilders({
+      'embeded': (pageName, params, _)=>EmbededFirstRouteWidget(),
+      'first': (pageName, params, _) {
+        print("xutao flutterPage params:$params");
+        return FirstRouteWidget();
+      },
+      'second': (pageName, params, _) => SecondRouteWidget(),
+      'tab': (pageName, params, _) => TabRouteWidget(),
+      'platformView': (pageName, params, _) => PlatformRouteWidget(),
+      'flutterFragment': (pageName, params, _) => FragmentRouteWidget(params),
+      ///可以在native层通过 getContainerParams 来传递参数
+      'flutterPage': (pageName, params, _) {
+        print("flutterPage params:$params");
+
+        return FlutterRouteWidget(params:params);
+      },
+    });
+    FlutterBoost.singleton.addBoostNavigatorObserver(TestBoostNavigatorObserver());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Boost example',
+        builder: FlutterBoost.init(postPush: _onRoutePushed),
+        home: Container(
+            color:Colors.white
+        ));
+  }
+
+  void _onRoutePushed(
+      String pageName, String uniqueId, Map params, Route route, Future _) {
+  }
+}
+class TestBoostNavigatorObserver extends NavigatorObserver{
+  void didPush(Route<dynamic> route, Route<dynamic> previousRoute) {
+
+    print("flutterboost#didPush");
+  }
+
+  void didPop(Route<dynamic> route, Route<dynamic> previousRoute) {
+    print("flutterboost#didPop");
+  }
+
+  void didRemove(Route<dynamic> route, Route<dynamic> previousRoute) {
+    print("flutterboost#didRemove");
+  }
+
+  void didReplace({Route<dynamic> newRoute, Route<dynamic> oldRoute}) {
+    print("flutterboost#didReplace");
+  }
+}
+
